7cac2b7dda3212f8fc64bd720071d53ba1d59329
ShovelTime <gousaid67@gmail.com>
2024-09-19 14:26:06 +1000

    Parameter name fix

--- src/utf16.h
+++ src/utf16.h
@@ -1,8 +1,8 @@
 #include "zig.h"
-zig_extern uint64_t utf16_to_utf8(uint64_t const a0, uint16_t const *const a1, uint8_t *const a2);
-zig_extern uint64_t utf8_to_utf16(uint64_t const a0, uint8_t const *const a1, uint16_t *const a2);
-zig_extern int32_t lfn_to_sfn_candidate(uint64_t const a0, uint16_t const *const a1, uint8_t *const a2);
-zig_extern int32_t is_sfn_compliant_utf16(uint64_t const a0, uint16_t const *const a1);
-zig_extern int32_t is_sfn_compliant_utf8(uint64_t const a0, uint8_t const *const a1);
-zig_extern uint64_t find_extension_utf8(uint64_t const a0, uint8_t const *const a1);
-zig_extern uintptr_t getauxval(uintptr_t const a0);
+zig_extern uint64_t utf16_to_utf8(uint64_t const in_len, uint16_t const *const in_string, uint8_t *const dest);
+zig_extern uint64_t utf8_to_utf16(uint64_t const in_len, uint8_t const *const in_string, uint16_t *const dest);
+zig_extern int32_t lfn_to_sfn_candidate(uint64_t const in_len, uint16_t const *const lfn_string, uint8_t *const dest);
+zig_extern int32_t is_sfn_compliant_utf16(uint64_t const len, uint16_t const *const string);
+zig_extern int32_t is_sfn_compliant_utf8(uint64_t const len, uint8_t const *const string);
+zig_extern uint64_t find_extension_utf8(uint64_t const len, uint8_t const *const string);
+zig_extern uintptr_t getauxval(uintptr_t const a0); // Huh?
